7d41206ad5b973d75d75ee5b49efd5204cc94afd
Peter Balog <peter@192.168.1.19>
2019-07-18 10:08:15 +0200

    testujeme ci to ide

---
+++ style.js
@@ -0,0 +1,47 @@
+
+
+
+
+// var i;
+
+// for(var i = 0; i<=10; i++){
+//     console.log(i)
+// }
+
+
+// var pocitadlo = ['auto','motorka'];
+
+// for( i = 0 ; i< pocitadlo.length; i++){
+//     console.log(pocitadlo[i]);
+
+// }
+
+
+var john = ["peto","adam","jozko"];
+
+for( var i = john.length-1 ; i>=0; i--){
+    console.log(john[i])
+}
+
+
+
+for(var i = 0; i<= john.length; i++){
+    console.log(john[i])
+}
+
+
+
+automat(1993);
+
+function automat(rok){
+
+    console.log(2019 - rok)
+}
+
+
+var nazov = document.getElementById("headertitle");
+console.log(nazov)
+
+nazov.textContent = "ahojteee"
+
+nazov.style.backgroundColor = "red";
